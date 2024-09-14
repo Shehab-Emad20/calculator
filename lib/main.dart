c348530da50280d304409579384ae85832f0304d
Shehab-Emad20 <shehabz2014@gmail.com>
2024-09-13 17:26:18 -0700

    creat project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const calculator());
+}
+
+class calculator extends StatelessWidget {
+  const calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
